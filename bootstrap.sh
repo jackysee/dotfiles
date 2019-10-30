cbdc962cde16eb14d5388f4420f5a2fbbdb958eb
jackysee <jackysee@gmail.com>
2019-10-30 14:33:14 +0800

    bootstrap scripts/ dir_colors / xinitrc

---
+++ bootstrap.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+echo "add ppa"
+sudo add-apt-repository ppa:jonathonf/vim -y
+
+echo "install apt packages"
+sudo apt install -qq \
+    build-essential \
+    zsh \
+    git \
+    htop \
+    tmux \
+    silversearh-ag \
+    less \
+    jq \
+    curl \
+    python3 \
+    python3-pip \
+    ruby-full \
+    tree \
+    wget \
+    vim-gtk \
+    --no-install-recommends \
+
+
+echo "copy fonts"
+if [ ! -f "~/.local/share/fonts" ]
+    mkdir -p ~/.local/share/fonts
+fi
+cp -r ~/.dotfiles/fonts/*.* ~/.local/share/fonts
+
+
+echo "install tpm"
+if [ ! -f "~/.dotfiles/tmux/.tmux/plugins" ]
+    mkdir -p ~/.dotfiles/tmux/.tmux/plugins
+fi
+git clone https://github.com/tmux-plugins/tpm ~/.dotfiles/tmux/.tmux/plugins/tpm
+
+
+echo "install tmuxinator"
+gem install tmuxinator
+
+
+echo "change default shell to zsh"
+chsh -s $(which zsh)
+
+
+echo "Relogin with zsh to install tools."
+echo "set up .gitconfig.local / .hgrc.local "
+echo "Open vim to install plugin via zplug."
